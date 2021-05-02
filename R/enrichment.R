3ac57f6b25e9b59c4d2a9cbecff84b4c94bb02e6
Haizi Zheng <haizi.zh@gmail.com>
2021-05-02 08:54:13 -0400

    Update the enrichment analysis

--- R/enrichment.R
+++ R/enrichment.R
@@ -1,74 +1,78 @@
+# Determine the anchor point
+get_anchor <- function(feature, anchor = c("center", "start", "end")) {
+  anchor = match.arg(anchor)
+  switch(
+    anchor,
+    center = as.integer(round(start(feature) + (width(
+      feature
+    ) - 1) / 2)),
+    start = start(feature),
+    end = end(feature),
+    stop(paste0("Invalid anchor: ", anchor))
+  )
+}
+
+
 #' @export
 enrichment_analysis <-
   function(hotspot,
            feature,
-           fix = c("center", "start", "end"),
+           anchor = c("center", "start", "end"),
            half_width = 1000L,
            flip_rev = TRUE) {
-    fix <- match.arg(fix)
-  mcols(hotspot) <- NULL
-  mcols(feature) <- NULL
-  common_seqlevels <- intersect(seqlevels(feature), seqlevels(hotspot))
-  feature <- keepSeqlevels(feature, common_seqlevels, pruning.mode = "coarse")
-  hotspot <- keepSeqlevels(hotspot, common_seqlevels, pruning.mode = "coarse")
+    anchor <- match.arg(anchor)
+    mcols(hotspot) <- NULL
+    mcols(feature) <- NULL
 
-  # Expand feature from mid point
-  if (fix == "center")
-    feature_mid_point <- as.integer(round(start(feature) + (width(feature) - 1) / 2))
-  else if (fix == "start")
-    feature_mid_point <- as.integer(start(feature))
-  else
-    feature_mid_point <- as.integer(end(feature))
+    # Only process the common part
+    common_seqlevels <- intersect(unique(seqnames(hotspot)),
+                                  unique(seqnames(feature)))
+    # common_seqlevels <-
+    #   intersect(seqlevels(feature), seqlevels(hotspot))
+    feature <-
+      keepSeqlevels(feature, common_seqlevels, pruning.mode = "coarse")
+    hotspot <-
+      keepSeqlevels(hotspot, common_seqlevels, pruning.mode = "coarse")
 
-  # Remove potential negative start positions
-  positive_feature_span <- feature_mid_point - half_width > 0
-  feature <- feature[positive_feature_span]
-  feature_mid_point <- feature_mid_point[positive_feature_span]
+    # Determine the anchor point
+    anchor_pos <- get_anchor(feature, anchor = anchor)
 
-  ranges(feature) <-
-    IRanges::IRanges(start = feature_mid_point - half_width,
-                     end = feature_mid_point + half_width)
-  hits <- findOverlaps(hotspot, feature)
+    # Adjusted feature: anchor - hw -> anchor + hw
+    ranges(feature) <-
+      IRanges::IRanges(start = pmax(1, anchor_pos - half_width),
+                       end = anchor_pos + half_width)
 
-  # Build the matching data frame between overlapping hotspot and feature
-  matched_gr <- hotspot[queryHits(hits)]
-  mcols(matched_gr) <- NULL
-  matched_gr$origin <- as.integer(start(feature[subjectHits(hits)]) + half_width)
+    hits <- findOverlaps(hotspot, feature)
 
-  strand(matched_gr) <- strand(feature[subjectHits(hits)])
+    # Build the matching data frame between overlapping hotspot and feature
+    matched_gr <- hotspot[queryHits(hits)]
+    matched_gr$anchor <- anchor_pos[subjectHits(hits)]
+    feature_strand <- strand(feature[subjectHits(hits)])
 
-  # # Some matched hotspots are large, or far away from the feature origin, thus
-  # # go beyond the [origin - half_window, origin + half_window] interval, so we
-  # # need to clip them.
-  # idx <- start(matched_gr) - matched_gr$origin < -half_width
-  # start(matched_gr[idx]) <- matched_gr$origin[idx] - half_width
-  # idx <- end(matched_gr) - matched_gr$origin > half_width
-  # end(matched_gr[idx]) <- matched_gr$origin[idx] + half_width
+    pseudo_origin <- 100e6L
+    ranges(matched_gr) <-
+      IRanges::IRanges(
+        start = ifelse(
+          feature_strand == "-" & flip_rev,
+          matched_gr$anchor - end(matched_gr),
+          start(matched_gr) - matched_gr$anchor
+        ) + pseudo_origin,
+        width = width(matched_gr)
+      )
+    seqnames(matched_gr) <- seqnames(matched_gr)[1]
 
-  # Build a pseudo BED using relative coordinates, so that we can count the "coverage"
-  pseudo_origin <- 10e6L + 1L
-  seqnames(matched_gr) <- seqnames(matched_gr)[1]
-  # flip - strand
-  flip_flag <- strand(matched_gr) == "-"
-  ranges(matched_gr) <-
-    IRanges::IRanges(
-      start = ifelse(flip_flag & flip_rev,
-                     matched_gr$origin - end(matched_gr) + pseudo_origin,
-                     start(matched_gr) - matched_gr$origin + pseudo_origin),
-      width = width(matched_gr)
+    scaffold <-
+      GenomicRanges::GRanges(seqnames = seqnames(matched_gr)[1],
+                             ranges = IRanges::IRanges(
+                               start = (pseudo_origin - half_width):(pseudo_origin + half_width),
+                               width = 1
+                             ))
+
+    data.table::data.table(
+      pos = seq(-half_width, half_width),
+      freq = countOverlaps(scaffold, matched_gr) / length(feature)
     )
-
-  scaffold <- GenomicRanges::GRanges(
-    seqnames = seqnames(matched_gr)[1],
-    ranges = IRanges::IRanges(start = (pseudo_origin - half_width):(pseudo_origin + half_width),
-                              width = 1)
-  )
-
-  data.table::data.table(
-    pos = seq(-half_width, half_width),
-    freq = countOverlaps(scaffold, matched_gr) / length(matched_gr)
-  )
-}
+  }
 
 
 #' @export
