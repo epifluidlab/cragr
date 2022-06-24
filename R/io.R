323c167e1040bfc520a7834825b55b8a6bc148a0
Haizi Zheng <haizi.zh@gmail.com>
2022-06-23 20:10:10 -0400

    Fix bug in loading fragment files

--- R/io.R
+++ R/io.R
@@ -7,29 +7,22 @@ read_fragments <- function(file_path, range = NULL, genome = NULL) {
   GenomicRanges::strand(frag) <- "*"
   logging::logdebug("Done reading BED data")
   frag_metadata <- mcols(frag)
-
-  mapq_guessed <- FALSE
-  if (!"mapq" %in% colnames(frag_metadata)) {
-    logging::logwarn("mapq not in column names. Need to guess which column is mapq")
-
-    for (col_idx in seq_along(colnames(frag_metadata))) {
-      if (is.integer(frag_metadata[[col_idx]])) {
-        logging::logwarn(str_interp("Column ${colnames(frag_metadata)[col_idx]} seems to be mapq"))
-        colnames(frag_metadata)[col_idx] <- "mapq"
-        mapq_guessed <- TRUE
-        break
-      }
+  
+  if ("mapq" %in% colnames(frag_metadata)) {
+    if (!is.integer(frag_metadata$mapq)) {
+      logging::logerror("MAPQ should be integers")
+      print(frag)
+      stop()
     }
-
-    if (!"mapq" %in% colnames(frag_metadata))
-      logging::logwarn("Cannot find mapq in the data frame")
+  } else if (ncol(frag_metadata) >= 2 && is.integer(frag_metadata[[2]])) {
+    frag_metadata$mapq <- frag_metadata[[2]]
+  } else {
+    logging::logwarn("Cannot find MAPQ in the data frame")
   }
-
+  
   # Only keep necessary columns
   meta_cols <- base::intersect(names(frag_metadata), c("mapq", "cigar1", "cigar2"))
   mcols(frag) <- frag_metadata[meta_cols]
-
-  GenomicRanges::strand(frag) <- "*"
   print(frag)
   frag
 }
